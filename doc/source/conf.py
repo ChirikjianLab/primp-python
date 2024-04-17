ed8dfccea91cfc098b655ceb018e747062d0b252
Sipu Ruan <ruansp@buaa.edu.cn>
2024-04-17 22:17:34 +0800

    Updated conf to compatible with github page

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,8 @@ release = 'v0.2.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc'
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages'
 ]
 
 templates_path = ['_templates']
